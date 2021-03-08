45cd2566f94fffcf4e2b14207c44e3492344cac4
L.J. Kraamer-Rietveld <loredana.rietveld@gmail.com>
2021-03-08 23:44:52 +0100

    const wordSoFar and log

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 function game(word, guesses) {
   console.log("Dit heb je tot nu toe geraden: ", guesses);
 
+  const wordSoFar = displayWordSoFar(word, guesses);
+  console.log(wordSoFar);
   const letter = question("Raad een letter: ");
 
   // voeg de geraden letter toe aan de array met guesses
