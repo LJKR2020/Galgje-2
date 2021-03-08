25cf98ab7fb55aa4a2a011ba62fa481bd6647081
L.J. Kraamer-Rietveld <loredana.rietveld@gmail.com>
2021-03-08 23:50:35 +0100

    word and guesses in game through wordSoFar

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 function game(word, guesses) {
   console.log("Dit heb je tot nu toe geraden: ", guesses);
 
-  const wordSoFar = displayWordSoFar(word, guesses);
-  console.log(wordSoFar);
+  const WordSoFar = displayWordSoFar(word, guesses);
+  console.log(WordSoFar);
   const letter = question("Raad een letter: ");
 
   // voeg de geraden letter toe aan de array met guesses
